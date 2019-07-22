73b77d4bb880fd3fbc3bd2a92dcb25daaa563450
shloimi minkowicz <shloimimink@gmail.com>
2019-07-21 22:48:37 -0500

    user-profile-context

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,21 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
+import React, {useState, useContext} from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/AlertContext';
 
 
-const Search = ({searchUsers, showClear, clearUsers, setAlert}) => {
+const Search = () => {
+    const githubContext = useContext(GithubContext);
+    const alertContext = useContext(AlertContext);
+
+
     const [text, setText] = useState('');
 
     const onSubmit = (e) => {
         e.preventDefault();
         if (text === '') {
-            setAlert('Please enter something', 'light');
+            alertContext.setAlert('Please enter something', 'light');
         } else {
-            searchUsers(text);
+            githubContext.searchUsers(text);
             setText('');
         }
     };
@@ -29,21 +34,14 @@ const Search = ({searchUsers, showClear, clearUsers, setAlert}) => {
                 />
                 <input type="submit" value="Search" className="btn btn-dark btn-block"/>
             </form>
-            {showClear && (
+            {githubContext.users.length > 0 && (
                 <button className="btn btn-light btn-block"
-                        onClick={clearUsers}>Clear
+                        onClick={githubContext.clearUsers}>Clear
                 </button>
             )}
 
         </div>
     );
-};
-
-Search.propTypes = {
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
 };
 
 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
 
-const Users = ({users, loading}) => {
+const Users = () => {
+    const githubContext = useContext(GithubContext);
+
+    const {loading, users} = githubContext;
+
     if (loading) {
         return <Spinner/>
     } else {
@@ -24,9 +28,5 @@ const userStyle = {
     gridGap: '1rem'
 };
 
-Users.propTypes = {
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired
-};
 
 export default Users;
